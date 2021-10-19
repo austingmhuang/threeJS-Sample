5a45aa25bfe54d56f5b6595be3ab370e67216057
Austin Huang <gengminghuang@gmail.com>
2021-10-19 09:30:10 +0900

    removed use-Gesture

--- src/components/Three/lights.js
+++ src/components/Three/lights.js
@@ -1,28 +1,28 @@
 // Component: Lights
 // Def: Lights for our canvas
 
-import React from "react";
+import React from 'react'
 
 const Lights = () => {
   return (
     <>
-      <fog attach='fog' args={["#fff", 0, 22]} />
+      <fog attach="fog" args={['#fff', 0, 22]} />
       <ambientLight intensity={0.4} />
       <directionalLight
         castShadow
         position={[-8, 16, -8]}
         intensity={0}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
+        shadow-mapSize-width={3000}
+        shadow-mapSize-height={3000}
         shadow-camera-far={50}
         shadow-camera-left={-10}
         shadow-camera-right={10}
         shadow-camera-top={10}
         shadow-camera-bottom={-10}
       />
-      <pointLight position={[0, 50, 0]} intensity={2} />
+      <pointLight position={[0, 50, 0]} intensity={3} />
     </>
-  );
-};
+  )
+}
 
-export default Lights;
+export default Lights
